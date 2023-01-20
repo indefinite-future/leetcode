7cb735d64ddcf225da1c747807d7abd31c6196ca
Alan Ho <taktingho@gmail.com>
2023-01-20 17:19:26 +0800

    first commit

---
+++ src/arraySolution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class arraySolution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        arraySolution sol = new arraySolution();
+        int[] array = {2, 11, 7, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(sol.twoSum(array, target)));
+    }
+}
